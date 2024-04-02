4fee09a15bb7536a24e8dbe7caf9fcd65ad7fd18
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2024-04-02 15:17:00 -0400

    feat: add search results

--- reddit_subreddit_posts.go
+++ reddit_subreddit_posts.go
@@ -44,6 +44,37 @@ func getRedditPosts(config *Config) ([]RedditPostResult, error) {
 	return results, nil
 }
 
+func getRedditSearchPosts(config *Config) ([]RedditPostResult, error) {
+	var results []RedditPostResult
+	var response RedditResponse
+	client := resty.New()
+	_, err := client.R().
+		SetHeaders(map[string]string{
+			"authority":                 "www.reddit.com",
+			"pragma":                    "no-cache",
+			"cache-control":             "no-cache",
+			"sec-ch-ua":                 `"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"`,
+			"sec-ch-ua-mobile":          "?0",
+			"upgrade-insecure-requests": "1",
+			"user-agent":                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+			"accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+			"dnt":                       "1",
+			"sec-fetch-site":            "none",
+			"sec-fetch-mode":            "navigate",
+			"sec-fetch-user":            "?1",
+			"sec-fetch-dest":            "document",
+			"accept-language":           "en-GB,en;q=0.9",
+		}).
+		SetResult(&response).
+		Get(fmt.Sprintf("https://www.reddit.com/search.json?q=%s&type=comment&sort=updated?limit=100", config.Tag))
+	if err != nil {
+		return results, err
+	}
+
+	results = append(results, response.Data.Children...)
+
+	return results, nil
+}
 func sendSlackNotificationForRedditPost(result RedditPostResult, config *Config) error {
 	if !config.NotifySlack {
 		return nil
@@ -95,6 +126,13 @@ func processRedditPosts(config *Config, db *gorm.DB) error {
 	if err != nil {
 		return err
 	}
+
+	searchResults, err := getRedditSearchPosts(config)
+	if err != nil {
+		return err
+	}
+
+	results = append(results, searchResults...)
 
 	inserted := 0
 	notified := 0
